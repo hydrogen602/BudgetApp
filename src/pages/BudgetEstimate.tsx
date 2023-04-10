77cdfa9b4e181bceb86da4166e39c28f155174b6
hydrogen602 <hydrogen31415@gmail.com>
2023-04-10 01:56:01 -0500

    progress?

--- src/pages/BudgetEstimate.tsx
+++ src/pages/BudgetEstimate.tsx
@@ -1,5 +1,6 @@
-import { Paper, Typography } from "@mui/material";
+import { AppBar, Button, IconButton, Paper, Toolbar, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import MenuIcon from '@mui/icons-material/Menu';
 import { CurrencyInput, PercentageInput } from "../components/NumericInput";
 import { Dinero } from "dinero.js";
 import DineroBuilder from "dinero.js";
@@ -9,6 +10,7 @@ import './BudgetEstimate.css';
 import { Chart } from 'react-chartjs-2';
 import { useState } from "react";
 import { FixedExpense, PercentExpense } from "../data";
+import { dialog } from "@tauri-apps/api";
 
 interface IExpenses {
   [key: string]: FixedExpense | PercentExpense;
@@ -31,72 +33,115 @@ function BudgetEstimate(props: {}) {
 
   console.log(netIncome, JSON.stringify(netIncome))
 
+  async function save() {
+    const options: dialog.SaveDialogOptions = {
+      title: 'Save File',
+      defaultPath: './',
+      filters: [
+        {
+          name: 'Json files',
+          extensions: ['json'],
+        },
+      ],
+    };
 
-  return <Box sx={{
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }}>
-    <Typography variant="h2">Budget Estimate</Typography>
+    const result = await dialog.save(options);
+    if (result) {
+      console.log(`File path: ${result}`);
+      // The user selected a file to save.
+      // Do something with the file path.
+    } else {
+      // The user canceled the save dialog.
+    }
+  }
 
-    <div className="main-paper-box">
-      <div className="sub-paper-box-1">
-        <Paper elevation={3} sx={{ padding: '1.5rem', margin: '1rem' }} className="paper-box">
-          <Typography variant="h4">Income</Typography>
-          <CurrencyInput sx={{
-            margin: '1rem',
-          }} props={{
-            label: 'Monthly Income',
-            size: 'small',
-          }} onChange={setIncome} />
-        </Paper>
+  return <>
+    <AppBar position="static" sx={{
+      minHeight: '2rem'
+    }}>
+      <Toolbar>
+        <IconButton
+          size="medium"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 1 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        {/* <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          News
+        </Typography> */}
+      </Toolbar>
+    </AppBar>
+    <Box sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}>
 
-        <Paper elevation={3} sx={{ padding: '1.5rem', margin: '1rem' }} className="paper-box">
-          <Typography variant="h4">Expenses</Typography>
+      <Typography variant="h2">Budget Estimate</Typography>
+      <Button onClick={save}>Stuff</Button>
 
-          {Object.entries(expenses).map(([name, expensive]) => {
-            if (expensive instanceof FixedExpense) {
-              return <CurrencyInput key={name} sx={{
-                margin: '1rem',
-              }} props={{
-                label: name,
-                size: 'small',
-              }} onChange={amount => setExpenses(allExpenses => { return { ...allExpenses, [name]: new FixedExpense(amount) } })} />;
-            } else if (expensive instanceof PercentExpense) {
-              return <PercentageInput key={name} sx={{
-                margin: '1rem',
-              }} props={{
-                label: name,
-                size: 'small',
-              }} onChange={amount => setExpenses(allExpenses => { return { ...allExpenses, [name]: new PercentExpense(amount) } })} />;
-            }
-          })}
-        </Paper>
+      <div className="main-paper-box">
+        <div className="sub-paper-box-1">
+          <Paper elevation={3} sx={{ padding: '1.5rem', margin: '1rem' }} className="paper-box">
+            <Typography variant="h4">Income</Typography>
+            <CurrencyInput sx={{
+              margin: '1rem',
+            }} props={{
+              label: 'Monthly Income',
+              size: 'small',
+            }} onChange={setIncome} />
+          </Paper>
+
+          <Paper elevation={3} sx={{ padding: '1.5rem', margin: '1rem' }} className="paper-box">
+            <Typography variant="h4">Expenses</Typography>
+
+            {Object.entries(expenses).map(([name, expensive]) => {
+              if (expensive instanceof FixedExpense) {
+                return <CurrencyInput key={name} sx={{
+                  margin: '1rem',
+                }} props={{
+                  label: name,
+                  size: 'small',
+                }} onChange={amount => setExpenses(allExpenses => { return { ...allExpenses, [name]: new FixedExpense(amount) } })} />;
+              } else if (expensive instanceof PercentExpense) {
+                return <PercentageInput key={name} sx={{
+                  margin: '1rem',
+                }} props={{
+                  label: name,
+                  size: 'small',
+                }} onChange={amount => setExpenses(allExpenses => { return { ...allExpenses, [name]: new PercentExpense(amount) } })} />;
+              }
+            })}
+          </Paper>
+        </div>
+        <div className="sub-paper-box-2">
+          <Paper elevation={3} sx={{ padding: '1.5rem', margin: '1rem' }}>
+            <Typography variant="h4">Net Income</Typography>
+            <Typography variant="h4">{netIncome.toFormat('$0,0.00')}</Typography>
+
+            <div className="donut-chart-box">
+              <Chart className="donut-chart" type='doughnut' data={{
+                labels: [...Object.keys(expenses), 'Net Income'],
+                datasets: [
+                  {
+                    label: 'Expenses',
+                    data: [...Object.entries(expenses).map(([_, expense]) => expense.getAmount(income).getAmount() / 100), netIncome.getAmount() / 100],
+                  }
+                ]
+              }}
+
+              />
+            </div>
+          </Paper>
+        </div>
       </div>
-      <div className="sub-paper-box-2">
-        <Paper elevation={3} sx={{ padding: '1.5rem', margin: '1rem' }}>
-          <Typography variant="h4">Net Income</Typography>
-          <Typography variant="h4">{netIncome.toFormat('$0,0.00')}</Typography>
 
-          <div className="donut-chart-box">
-            <Chart className="donut-chart" type='doughnut' data={{
-              labels: [...Object.keys(expenses), 'Net Income'],
-              datasets: [
-                {
-                  label: 'Expenses',
-                  data: [...Object.entries(expenses).map(([_, expense]) => expense.getAmount(income).getAmount() / 100), netIncome.getAmount() / 100],
-                }
-              ]
-            }}
-
-            />
-          </div>
-        </Paper>
-      </div>
-    </div>
-
-  </Box>;
+    </Box>
+  </>;
 }
 
 export default BudgetEstimate;
--- src/rust-invoke.ts
+++ src/rust-invoke.ts
@@ -1,6 +1,14 @@
 import { invoke } from "@tauri-apps/api";
 import { UpdateBudgetJson } from "./rust-types/UpdateBudgetJson";
 
-async function update_budget(update: UpdateBudgetJson): Promise<void> {
+async function updateBudget(update: UpdateBudgetJson): Promise<void> {
   return await invoke("update_budget", { update });
+}
+
+async function saveToDisk(path: String) {
+  return await invoke("save_to_disk", { path });
+}
+
+async function loadToDisk(path: String) {
+  return await invoke("load_from_disk", { path });
 }
